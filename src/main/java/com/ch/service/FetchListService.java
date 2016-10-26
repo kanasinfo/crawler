85d56dd788024ec72a4bcf3a9f14bd7e3473be90
gefangshuai <gefangshuai@163.com>
2016-10-26 21:35:58 +0800

    fetch coding

--- src/main/java/com/ch/service/FetchListService.java
+++ src/main/java/com/ch/service/FetchListService.java
@@ -12,15 +12,23 @@ import java.util.ListIterator;
  * Created by Devid on 2016/10/26.
  */
 public class FetchListService {
+    private static final String QUERY_URL = "/query?q=&s=ka&s=sr&s=jr&s=mr&s=accept360&p=kwbw&start=";
+
     public String fetchPage(String url) throws IOException {
         Document document = FetchUtils.getByUrl(url);
         String pageFun = document.select("div[align='center'] script").outerHtml();
         System.out.println("pageFun: " + pageFun);
 //        TODO
+
         String funParam = "'184163','15','1','/query?q=&s=ka&s=sr&s=jr&s=mr&s=accept360&p=kwbw&start='".replace("'", "");
         String[] params = StringUtils.split(",");
 
         return null;
+    }
+
+    private Document listPage(String url, int start){
+        String fetchUrl = PropUtils.getProp("url.server") + QUERY_URL + start;
+        FetchUtils.getByUrl(fetchUrl)
     }
 
     public void fetchList(String url) throws IOException {
